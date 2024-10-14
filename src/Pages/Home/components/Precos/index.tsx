7ad3bc773b17c345d4317eefc77d3fde04d6f006
ericAleixo <ericaleixo66@gmail.com>
2024-10-14 12:13:58 -0300

    Fix: precos renderização de cards

--- src/Pages/Home/components/Precos/index.tsx
+++ src/Pages/Home/components/Precos/index.tsx
@@ -4,49 +4,64 @@ import imagemIttalo from "./assets/ittalo.JPG"
 import imagemAyrton from "./assets/ayrton.JPG"
 import imagemAdriana from "./assets/adriana.jpg"
 
-export const Precos = () =>{
-    return(
-        <div className="bg-white py-6 sm:py-8 lg:py-12">
-        <div className="mx-auto max-w-screen-2xl px-4 md:px-8" id="colecoes">
-          <div className="mb-6 flex items-end justify-between gap-4">
-            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">Coleções</h2>
+export const Precos = () => {
 
-            <a href="#" className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base">Mostre mais</a>
-          </div>
+  const cards = [
+    {
+      id: 1,
+      imagem: imagemIttalo,
+      genero: "Masculino",
+      tipo: "Negócios Casuais",
+      descricao: "Foto de roupas de negócios casuais"
+    },
+    {
+      id: 2,
+      imagem: imagemAyrton,
+      genero: "Masculino",
+      tipo: "Negócios Casuais",
+      descricao: "Foto de roupas de moda de rua"
+    },
+    {
+      id: 3,
+      imagem: imagemAdriana,
+      genero: "Feminino",
+      tipo: "Festas Sociais",
+      descricao: "Foto de roupas para festas sociais"
+    },
+    {
+      id: 4,
+      imagem: imagemIttalo,
+      genero: "Masculino",
+      tipo: "Negócios Casuais",
+      descricao: "Foto de roupas"
+    }
+  ]
 
-          <div className="grid gap-x-4 gap-y-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
+  return (
+    <div className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8" id="colecoes">
+        <div className="mb-6 flex items-end justify-between gap-4">
+          <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">Coleções</h2>
 
-            <CardTipo
-            imagem = {imagemIttalo}
-            genero = "Masculino"
-            tipo="negócios casuais"
-            descricao="Foto de roupas para negócios casuais">
-            </CardTipo>
+          <a href="#" className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base">Mostre mais</a>
+        </div>
 
-            <CardTipo
-            imagem = {imagemAdriana}
-            genero = "Feminino"
-            tipo="festas sociais"
-            descricao="Foto de roupas para Festas sociais">
-            </CardTipo>
+        <div className="grid gap-x-4 gap-y-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
 
-            <CardTipo
-            imagem = {imagemAyrton}
-            genero = "Masculino"
-            tipo="moda de rua"
-            descricao="Foto de roupas para uso urbano">
-            </CardTipo>
 
-            <CardTipo
-            imagem = {imagemIttalo}
-            genero = "Masculino"
-            tipo="negócios casuais"
-            descricao="Foto de roupas para negócios casuais">
-            </CardTipo>
+          {
+            cards.map(card => (
+              <CardTipo
+                imagem={card.imagem}
+                genero={card.genero}
+                tipo={card.genero}
+                descricao={card.descricao}>
+              </CardTipo>
+            ))
+          }
 
-            
-          </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
