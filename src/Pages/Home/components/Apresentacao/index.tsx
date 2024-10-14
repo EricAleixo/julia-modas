e4abe3bb01a3fe60f114dfbc11487c0d44415aba
ericAleixo <ericaleixo66@gmail.com>
2024-10-14 10:13:12 -0300

    Animate: apresentacao animada

--- src/Pages/Home/components/Apresentacao/index.tsx
+++ src/Pages/Home/components/Apresentacao/index.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion"
+
 import imagemAnael from "./assets/anael.JPG"
 import imagemValter from "./assets/valter.JPG"
 
@@ -7,20 +9,44 @@ export const Apresentacao = () => {
     return (
         <section className="mx-auto max-w-screen-2xl px-4 md:px-8" id="apresentacao">
             <div className="mb-8 flex flex-wrap justify-between md:mb-16">
-                <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
+                <motion.div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48"
+                initial = {{x: -1000, opacity: 0}}
+                animate = {{x: 0, opacity:1}}
+                transition={{
+                    type:"spring",
+                    damping: 20,
+                    stiffness: 120,
+                    ease: "easeInOut"
+                }}>
                     <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">Descubra o Estilo<br />que é a Sua Cara!</h1>
 
                     <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">Bem-vindo à Júlia, onde a moda encontra a sua essência! Aqui, cada peça é cuidadosamente selecionada para refletir tendências contemporâneas e o seu estilo único.</p>
-                </div>
+                </motion.div>
 
                 <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-                    <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
+                    <motion.div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0"
+                    initial = {{x: 1000, opacity: 0}}
+                    animate = {{x: 0, opacity:1}}
+                    transition={{
+                        type:"spring",
+                        damping: 21,
+                        stiffness: 65,
+                        ease: "easeInOut",
+                    }}>
                         <img src={imagemAnael} alt="Foto de Anael" className="h-full w-full object-cover object-center" />
-                    </div>
+                    </motion.div>
 
-                    <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
+                    <motion.div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg"
+                    initial = {{x: -1000, opacity: 0}}
+                    animate = {{x: 0, opacity:1}}
+                    transition={{
+                        type:"spring",
+                        damping: 21,
+                        stiffness: 65,
+                        ease: "easeInOut",
+                    }}>
                         <img src={imagemValter} alt="Foto de Valter" className="h-full w-full object-cover object-center" />
-                    </div>
+                    </motion.div>
                 </div>
             </div>
 
