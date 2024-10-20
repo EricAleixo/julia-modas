fb4e2d8bd2aec3c9da2d0f64a5aca4bda002acad
ericAleixo <ericaleixo66@gmail.com>
2024-10-20 14:59:30 -0300

    Fix: fechar notificacao

--- src/shared/Notificacao/index.tsx
+++ src/shared/Notificacao/index.tsx
@@ -1,9 +1,12 @@
 import { motion } from "framer-motion"
 
 export const Notificacao = () => {
-
+  
     const fecharBotao = () =>{
-        console.log("Funciona")
+        const cardNotificacao = document.querySelector("#marketing-banner") as HTMLDivElement
+        if(cardNotificacao){
+            cardNotificacao.style.display = "none"
+        }
     }
 
     return (
@@ -18,12 +21,11 @@ export const Notificacao = () => {
             </div>
             <div className="flex items-center flex-shrink-0">
                 <a href="#" className="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none">Acesse</a>
-                <button data-dismiss-target="#marketing-banner" type="button" className="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5">
+                <button data-dismiss-target="#marketing-banner" type="button" className="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5" onClick={fecharBotao}>
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
-                    <span className="sr-only"
-                    onClick={fecharBotao}>Feche o banner</span>
+                    <span className="sr-only">Feche o banner</span>
                 </button>
             </div>
         </motion.div>
