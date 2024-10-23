f895f68ce467a2f527b642b38ab640c8040336ff
ericAleixo <ericaleixo66@gmail.com>
2024-10-23 13:08:31 -0300

    Fix: condicional vip

--- src/Pages/ContaPage/Components/DadosCliente/index.tsx
+++ src/Pages/ContaPage/Components/DadosCliente/index.tsx
@@ -14,7 +14,7 @@ interface DadosClientesProps {
 
 
 
-export const DadosCliente: React.FC<DadosClientesProps> = ({id, nome, email, senha, vip, totalCompras, data_criacao }) => {
+export const DadosCliente: React.FC<DadosClientesProps> = ({ id, nome, email, senha, vip, totalCompras, data_criacao }) => {
 
 
     return (
@@ -49,10 +49,13 @@ export const DadosCliente: React.FC<DadosClientesProps> = ({id, nome, email, sen
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                         </svg>
                         PRO
-                    </span> : <span className="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full">
+                    </span> : <span className="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-red-200 text-red-800 rounded-full">
                         <svg className="size-2.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 0 1 .708 0L8 6.586l1.938-1.94a.5.5 0 1 1 .708.708L8.707 7.293l1.94 1.938a.5.5 0 1 1-.708.708L8 8.707l-1.938 1.94a.5.5 0 1 1-.708-.708l1.94-1.938-1.94-1.94a.5.5 0 0 1 0-.708z" />
                         </svg>
+
+
+
                         Não ativado
                     </span>}
                 </div>
