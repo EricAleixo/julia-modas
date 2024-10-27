5b2d80f4718c35ce864bd07232ae14c80e47a44c
ericAleixo <ericaleixo66@gmail.com>
2024-10-27 09:48:14 -0300

    Fix: modal open && close

---
+++ src/Pages/ContaPage/Components/Modal/index.tsx
@@ -0,0 +1,86 @@
+interface ModalProps{
+
+    modalStatus: boolean
+    setIsOpen: unknown
+
+}
+
+export const Modal: React.FC<ModalProps> = ({modalStatus, setIsOpen}) => {
+
+    if(modalStatus){
+
+        return (
+            <div id="modal" className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
+                <div className="bg-white rounded-lg shadow-lg w-11/12 md:w-1/2 p-6">
+    
+                    <div className="bg-white rounded-xl shadow p-4 sm:p-7">
+                        <div className="text-center mb-8">
+                            <h2 className="text-2xl md:text-3xl font-bold text-gray-900">Novo cliente</h2>
+                            <p className="text-sm text-gray-600">
+                                Adicione um novo cliente e o configure diretamente
+                            </p>
+                        </div>
+    
+                        <form action="#">
+                            <div className="mt-2 space-y-3">
+                                <input
+                                    type="text"
+                                    className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                                    placeholder="Nome"
+                                />
+                                <input
+                                    type="text"
+                                    className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                                    placeholder="Email"
+                                />
+                                <div className="flex flex-col sm:flex-row gap-3">
+                                    <input
+                                        type="text"
+                                        className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                                        placeholder="Senha do usuário"
+                                    />
+                                    <input
+                                        type="text"
+                                        className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                                        placeholder="Confirme a senha"
+                                    />
+                                    <select
+                                        className="py-2 px-3 pe-9 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                                    >
+                                        <option selected>VIP</option>
+                                        <option>Nenhum</option>
+                                        <option>Básico</option>
+                                        <option>Pro</option>
+                                    </select>
+                                </div>
+                                <input
+                                    type="number"
+                                    className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                                    placeholder="Total de compras"
+                                />
+                            </div>
+                        </form>
+    
+                        <div className="mt-5 flex justify-end gap-x-2">
+                            <button
+                                type="button"
+                                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                                onClick={() => setIsOpen(!modalStatus)}
+                            >
+                                Cancelar
+                            </button>
+                            <button
+                                type="button"
+                                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                            >
+                                Salvar mudanças
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+        )
+    }
+    return null
+}
--- src/Pages/ContaPage/index.tsx
+++ src/Pages/ContaPage/index.tsx
@@ -1,7 +1,7 @@
 import { Cabecalho } from "../../shared/Cabecalho"
 import { Rodape } from "../../shared/Rodape"
-
 import { DadosCliente } from "./Components/DadosCliente"
+import { Modal } from "./Components/Modal"
 
 import { useEffect, useState } from "react"
 
@@ -10,13 +10,13 @@ import { api } from "../../services/api"
 
 interface Client {
 
-    id: number,
-    nome: string,
-    email: string,
-    senha: string,
-    vip: boolean,
-    totalCompras: number,
-    data_criacao: string
+    id: number;
+    nome: string;
+    email: string;
+    senha: string;
+    vip: boolean;
+    totalCompras: number;
+    data_criacao: string;
 
 }
 
@@ -24,19 +24,23 @@ export const ContaPage = () => {
 
     const [clients, setClients] = useState<Client[]>([])
 
-    const getClietns = async () => {
-        const clientsFull = await api.get("/client")
-        setClients(clientsFull.data)
-
+    const getClients = async () => {
+        const response = await api.get("/client");
+        if (Array.isArray(response.data.query)) {
+            setClients(response.data.query);
+        } 
     }
+    
 
     useEffect(() => {
-        getClietns()
+        getClients()
     }, [])
+
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <div>
-            <Cabecalho></Cabecalho>
+            <Cabecalho />
             <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
                 <div className="flex flex-col">
                     <div className="-m-1.5 overflow-x-auto">
@@ -44,22 +48,24 @@ export const ContaPage = () => {
                             <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden">
                                 <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200">
                                     <div>
-                                        <h2 className="text-xl font-semibold text-gray-800">
-                                            Usuários
-                                        </h2>
-                                        <p className="text-sm text-gray-600">
-                                            Adicione usuários, edite e mais
-                                        </p>
+                                        <h2 className="text-xl font-semibold text-gray-800">Usuários</h2>
+                                        <p className="text-sm text-gray-600">Adicione usuários, edite e mais</p>
                                     </div>
                                     <div>
                                         <div className="inline-flex gap-x-2">
-                                            <a className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50" href="#">
+                                            <a className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50" href="#">
                                                 Veja todos
                                             </a>
-                                            <a className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="#">
-                                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
+                                            <button className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700"
+onClick={() => setIsOpen(!isOpen)}                                            >
+                                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                                    <path d="M5 12h14" /><path d="M12 5v14" />
+                                                </svg>
                                                 Adicionar Usuário
-                                            </a>
+                                            </button>
+
+                                            <Modal modalStatus = {isOpen} setIsOpen = {setIsOpen}></Modal>
+
                                         </div>
                                     </div>
                                 </div>
@@ -68,79 +74,72 @@ export const ContaPage = () => {
                                         <tr>
                                             <th scope="col" className="ps-6 py-3 text-start">
                                                 <label htmlFor="hs-at-with-checkboxes-main" className="flex">
-                                                    <input type="checkbox" className="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" id="hs-at-with-checkboxes-main" />
+                                                    <input type="checkbox" className="shrink-0 border-gray-300 rounded text-blue-600" id="hs-at-with-checkboxes-main" />
                                                     <span className="sr-only">Checkbox</span>
                                                 </label>
                                             </th>
                                             <th scope="col" className="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3 text-start">
                                                 <div className="flex items-center gap-x-2">
-                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">
-                                                        Nome
-                                                    </span>
+                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">Nome</span>
                                                 </div>
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-start">
                                                 <div className="flex items-center gap-x-2">
-                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">
-                                                        Senha
-                                                    </span>
+                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">Senha</span>
                                                 </div>
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-start">
                                                 <div className="flex items-center gap-x-2">
-                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">
-                                                        Vip
-                                                    </span>
+                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">Vip</span>
                                                 </div>
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-start">
                                                 <div className="flex items-center gap-x-2">
-                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">
-                                                        Total de compras
-                                                    </span>
+                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">Total de compras</span>
                                                 </div>
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-start">
                                                 <div className="flex items-center gap-x-2">
-                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">
-                                                        Data de criação
-                                                    </span>
+                                                    <span className="text-xs font-semibold uppercase tracking-wide text-gray-800">Data de criação</span>
                                                 </div>
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-end"></th>
                                         </tr>
                                     </thead>
                                     <tbody className="divide-y divide-gray-200">
-
-                                        {clients.map(() => (
+                                        {clients.map(client => (
                                             <DadosCliente
-                                                id="wdiojqd"
-                                                nome="wdiojqd"
-                                                email="wdiojqd"
-                                                senha="wdiojqd"
-                                                vip={false}
-                                                totalCompras={22}
-                                                data_criacao="wdiojqd">
-                                            </DadosCliente>
+                                                key={client.id}
+                                                id={String(client.id)}
+                                                nome={client.nome}
+                                                email={client.email}
+                                                senha={client.senha}
+                                                vip={client.vip}
+                                                totalCompras={client.totalCompras}
+                                                data_criacao={client.data_criacao}
+                                            />
                                         ))}
-
                                     </tbody>
                                 </table>
                                 <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200">
                                     <div>
                                         <p className="text-sm text-gray-600">
-                                            <span className="font-semibold text-gray-800">0</span> resultados
+                                            <span className="font-semibold text-gray-800">{clients.length}</span> resultados
                                         </p>
                                     </div>
                                     <div>
                                         <div className="inline-flex gap-x-2">
-                                            <button type="button" className="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50">
-                                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6" /></svg>
+                                            <button type="button" className="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50">
+                                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                                    <path d="m15 18-6-6 6-6" />
+                                                </svg>
                                                 Anterior
                                             </button>
-                                            <button type="button" className="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50">
+                                            <button type="button" className="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50">
                                                 Próximo
-                                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg>
+                                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                                    <path d="m9 18 6-6-6-6" />
+                                                </svg>
                                             </button>
                                         </div>
                                     </div>
@@ -150,8 +149,7 @@ export const ContaPage = () => {
                     </div>
                 </div>
             </div>
-            <Rodape></Rodape>
+            <Rodape />
         </div>
     )
-
 }
