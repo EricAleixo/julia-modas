b341fae58aa871ff2fdecafe3c56ab90a79730fa
ericAleixo <ericaleixo66@gmail.com>
2024-10-28 12:26:35 -0300

    Fix: URL ajeitada

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://api-julia-modas-re71n3qxn-erics-projects-470b2ceb.vercel.app/"
+    baseURL: "https://api-julia-modas.vercel.app"
 })
